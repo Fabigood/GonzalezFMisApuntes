895c976c4787f6ee36f25473a0a4675421006076
Fabio Gonzalez <fabio.gonzalez.fiallos@udla.edu.ec>
2025-06-11 21:14:04 -0500

    Add project files.

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
